f34c803f9051587fba2780ca31ae60281f73984a
Vashiv1 <vashiv@outlook.com>
2025-08-28 08:36:15 +0200

    Created and openeD new file

---
+++ ConstructionWeb/ConstructionWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using ConstructionWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstructionWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
